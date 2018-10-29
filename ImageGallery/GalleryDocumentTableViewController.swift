8e29d9b2b94427d9d663ff791c6d57ae1b4e3f3b
Maulik Sharma <mauliksharma@Mauliks-MacBook.local>
2018-10-29 08:48:18 +0530

    Implemented Persistence, Refactored

--- ImageGallery/GalleryDocumentTableViewController.swift
+++ ImageGallery/GalleryDocumentTableViewController.swift
@@ -14,16 +14,6 @@ class GalleryDocumentTableViewController: UITableViewController {
     
     var recentlyDeletedDocuments = [String]()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
